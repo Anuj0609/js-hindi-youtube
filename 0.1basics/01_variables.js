dbba6eb64c46482a1054e942ebd3c1ed139d7b8a
Anuj0609 <122191546+Anuj0609@users.noreply.github.com>
2024-01-18 09:29:00 +0000

    dataTypes

---
+++ 0.1basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=123356
+let accountEmail="anujshrivastava982@gmail.com"
+var accountPassword="123456"
+accountCity="Jaipur"
+
+// accountId=2 // not allowed
+accountEmail ="zaynmyst@gmail.com"
+accountPassword="098765"
+accountCity="Delhi"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
