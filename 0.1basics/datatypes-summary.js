fab12397142fb486a5dcd3f841675660f4d02ed6
Anuj0609 <122191546+Anuj0609@users.noreply.github.com>
2024-01-24 14:28:38 +0000

    summary

---
+++ 0.1basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// # Primitive
+
+//7 types:String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsidetemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id===anotherId);
+
+
+const bigNumber= 75341658713646843865843015n
+
+// Reference type (Non primitive)
+
+//Arrey, Objects, Functions
+
+const heroes = ["saktiman", "naagraaj", "doga"]
+
+let myObj=
+{
+    name:"Anuj",
+    age:22,
+}
+
+const myFunction = fundtion(){
+    console.log("Hello World");
+}
+
+console.log(typeOf bigNumber);
