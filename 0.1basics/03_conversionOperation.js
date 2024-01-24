161a8093914018507b647b9d345a7d7258347256
Anuj0609 <122191546+Anuj0609@users.noreply.github.com>
2024-01-24 13:43:16 +0000

    conversion

--- 0.1basics/03_conversionOperation.js
+++ 0.1basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score= null
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33"==>33
 //"33abc"==>Nan
@@ -12,7 +12,7 @@ console.log(valueInNumber);
 
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //"=>false
@@ -21,6 +21,38 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 let stringNumber= String(someNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
 
+//********Operatios*******************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2)
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="Hello"
+let str2=" World"
+
+let str3= str1+str2
+
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)
+ 
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
